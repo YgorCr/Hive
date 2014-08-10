7048b6edb71755028019c446116ca49dfd92259b
andy <andygajadhar@gmail.com>
2014-08-09 23:42:25 -0300

    testelistaingresso

--- src/gui/IngressoBean.java
+++ src/gui/IngressoBean.java
@@ -1,7 +1,12 @@
 package gui;
 
+import java.util.Calendar;
+import java.util.HashMap;
+
 import javax.faces.bean.ManagedBean;
 
+import util.DataDeValidadeException;
+import util.PrecoException;
 import business.controllers.IngressoController;
 import business.controllers.IngressoControllerIF;
 import business.model.IngressoAB;
@@ -12,6 +17,24 @@ public class IngressoBean {
 	private IngressoControllerIF controller = new IngressoController();
 	
 	public IngressoAB[] getList(){
+		HashMap<String, Object> obj = new HashMap<String, Object>();
+		obj.put("idEvento", 1);
+		obj.put("valor", 3.25);
+		Calendar umDiaQualquer = Calendar.getInstance();
+		umDiaQualquer.set(Calendar.YEAR, 2015);
+		obj.put("dataDeValidade", umDiaQualquer);
+		try {
+			controller.create(obj);
+		} catch (PrecoException e) {
+			// TODO Auto-generated catch block
+			System.out.println("NAOAAAAAAA");
+			e.printStackTrace();
+		} catch (DataDeValidadeException e) {
+			// TODO Auto-generated catch block
+			System.out.println("NAOAAAAAAA");
+			e.printStackTrace();
+		}
+		
 		return controller.listAll();
 	}
 	
