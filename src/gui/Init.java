85758186fdc77bb69dc362e234ec55124867a88e
andy <andygajadhar@gmail.com>
2014-08-09 23:51:34 -0300

    adicionado populamento de ingressos

--- src/gui/Init.java
+++ src/gui/Init.java
@@ -1,26 +1,32 @@
 package gui;
 
+import java.util.Calendar;
 import java.util.HashMap;
 
 import javax.faces.bean.ManagedBean;
 
+import business.controllers.IngressoController;
+import business.controllers.IngressoControllerIF;
 import business.controllers.UsuarioController;
 import business.controllers.UsuarioControllerIF;
+import util.DataDeValidadeException;
 import util.EmailException;
 import util.IdadeException;
 import util.LoginException;
 import util.NomeException;
+import util.PrecoException;
 import util.SenhaException;
 
 @ManagedBean
 public class Init {
 	private static UsuarioControllerIF userController = new UsuarioController();
 	private static Boolean rodou = false;
-	
+	private static IngressoControllerIF ingrController = new IngressoController();
+
 	public String getTesteInit(){
 		if(!rodou){
 			initUsuarios();
-			
+			initIngressos();
 			rodou = true;
 		}
 		
@@ -37,6 +43,29 @@ public class Init {
 			userController.create(user);
 		} catch (EmailException | IdadeException | LoginException
 				| NomeException | SenhaException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void initIngressos(){
+		HashMap<String, Object> obj = new HashMap<String, Object>();
+		obj.put("idEvento", 1);
+		obj.put("valor", 3.25);
+		Calendar umDiaQualquer = Calendar.getInstance();
+		umDiaQualquer.set(Calendar.YEAR, 2015);
+		obj.put("dataDeValidade", umDiaQualquer);
+		try {
+			ingrController.create(obj);
+			ingrController.create(obj);
+			ingrController.create(obj);
+			ingrController.create(obj);
+		} catch (PrecoException e) {
+			// TODO Auto-generated catch block
+			System.out.println("NAOAAAAAAA");
+			e.printStackTrace();
+		} catch (DataDeValidadeException e) {
+			// TODO Auto-generated catch block
+			System.out.println("NAOAAAAAAA");
 			e.printStackTrace();
 		}
 	}
