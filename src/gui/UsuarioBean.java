b2e7bc0e82100a5ec20e79d6af1e0572e9f3e0ea
YgorCr <ygor.crispim@gmail.com>
2014-08-10 21:52:40 -0300

    só uma ideia.

--- src/gui/UsuarioBean.java
+++ src/gui/UsuarioBean.java
@@ -15,17 +15,28 @@ import business.controllers.UsuarioController;
 
 @ManagedBean(name = "usuario")
 public class UsuarioBean {
-	private UsuarioAB user;
+	private UsuarioAB user = new UsuarioAB() {};
 	private UsuarioControllerIF controller = new UsuarioController();
 	
-	public Long create(String nome, String email, String telefone, String idade, String cpf, String sexo){
+	public Long create(){	
+//		public Long create(String nome, String email, String telefone, String idade, String cpf, String sexo){
 		HashMap<String, Object> newUser = new HashMap<String, Object>();
-		newUser.put("nome", nome);
-		newUser.put("email", email);
-		newUser.put("telefone", telefone);
-		newUser.put("idade", Long.parseLong(idade));
-		newUser.put("cpf", cpf);
-		newUser.put("sexo", sexo);
+//		newUser.put("nome", nome);
+//		newUser.put("email", email);
+//		newUser.put("telefone", telefone);
+//		newUser.put("idade", Long.parseLong(idade));
+//		newUser.put("cpf", cpf);
+//		newUser.put("sexo", sexo);
+		System.out.println("GREATE ops create");
+		System.out.println("email: " + this.user.getEmail());
+		System.out.println("nome: "+ this.user.getNome());
+		
+		newUser.put("nome", user.getNome());
+		newUser.put("email", user.getEmail());
+		newUser.put("telefone", user.getTelefone());
+		newUser.put("idade", user.getIdade());
+		newUser.put("cpf", user.getCpf());
+		newUser.put("sexo", user.getSexo());
 		
 		Long id = null;
 		try {
@@ -50,5 +61,61 @@ public class UsuarioBean {
 	
 	public UsuarioAB[] getList(Long max, Long offset){
 		return controller.listAll(offset, max);
+	}
+	
+	public void setNome(String nome){
+		System.out.println("set Working: " + nome);
+		this.user.setNome(nome);
+	}
+	
+	public void setCpf(String cpf){
+		this.user.setCpf(cpf);
+	}
+	
+	public void setEmail(String email){
+		System.out.println("set Working: " + email);
+		this.user.setEmail(email);
+	}
+	
+	public void setIdade(String idade){
+		this.user.setIdade(Integer.parseInt(idade));
+	}
+	
+	public void setSexo(String sexo){
+		this.user.setSexo(sexo);
+	}
+	
+	public void setTelefone(String telefone){
+		this.user.setTelefone(telefone);
+	}
+	
+	public String getCpf(){
+		return this.user.getCpf();
+	}
+	
+	public String getEmail(){
+		System.out.println("get Working: " + this.user.getEmail());
+		return this.user.getEmail();
+	}
+	
+	public String getNome(){
+		System.out.println("get Working: " + this.user.getNome());
+		return this.user.getNome();
+	}
+	
+	public String getSexo(){
+		return this.user.getSexo();
+	}
+	
+	public String getTelefone(){
+		return this.user.getTelefone();
+	}
+	
+	public String getIdade(){
+		return this.user.getIdade().toString();
+	}
+	
+	public String getId(){
+		return this.user.getId().toString();
 	}
 }
