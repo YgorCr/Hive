4949c762a823c7796fe141a1d4b267465698e977
Italo Ayres <italoayres@gmail.com>
2014-08-22 14:50:58 -0300

    Switch persistencia no DaoAbstractFactory

--- src/infra/DaoAbstractFactory.java
+++ src/infra/DaoAbstractFactory.java
@@ -8,15 +8,28 @@ public class DaoAbstractFactory {
 	public static DaoIF<?> getInstance(Class<?> classe){
 		String classeName = classe.getName().split("\\.")[classe.getName().split("\\.").length-1];
 		
-		switch(classeName){
-			case "UsuarioAB":
-				return DaoUsuario.getInstance();
-			case "IngressoAB":
-				return DaoIngresso.getInstance();
-			case "EventoAB":
-				return DaoEvento.getInstance();
-		}
+		Boolean persistente = true; // Altere aqui para mudar o tipo de persistencia
 		
+		if (persistente) {
+			switch(classeName){
+				case "UsuarioAB":
+					return DaoUsuario.getInstance();
+				case "IngressoAB":
+					return DaoIngresso.getInstance();
+				case "EventoAB":
+					return DaoEvento.getInstance();
+			}
+		} else {
+			
+			switch(classeName){
+			case "UsuarioAB":
+				return DaoUsuarioMemo.getInstance();
+			case "IngressoAB":
+				return DaoIngressoMemo.getInstance();
+			case "EventoAB":
+				return DaoEventoMemo.getInstance();
+		}
+		}
 		return null;
 	}
 }
